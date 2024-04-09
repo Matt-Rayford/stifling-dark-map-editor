9b96bdb834b9455a1b682282300939a8d3611a2e
Matt <mattrayford8686@gmail.com>
2024-04-09 08:54:28 -0500

    Adding infinite image scroller

--- client/src/components/games/the-stifling-dark/investigator-viewer.tsx
+++ client/src/components/games/the-stifling-dark/investigator-viewer.tsx
@@ -2,15 +2,32 @@ import { investigators } from './investigators';
 
 export const InvestigatorViewer = () => {
 	return (
-		<div className='investigator-viewer'>
-			{investigators.map((investigator) => (
-				<img
-					width={300}
-					height={450}
-					src={investigator.imageSrc}
-					alt={`Portrait of ${investigator.name}`}
-				/>
-			))}
-		</div>
+		<>
+			<h2>Meet the Investigators</h2>
+			<div className='scroll-viewer'>
+				<div>
+					{investigators.map((investigator) => (
+						<img
+							key={`${investigator.name}-1`}
+							width={300}
+							height={450}
+							src={investigator.imageSrc}
+							alt={`Portrait of ${investigator.name}`}
+						/>
+					))}
+				</div>
+				<div>
+					{investigators.map((investigator) => (
+						<img
+							key={`${investigator.name}-2`}
+							width={300}
+							height={450}
+							src={investigator.imageSrc}
+							alt={`Portrait of ${investigator.name}`}
+						/>
+					))}
+				</div>
+			</div>
+		</>
 	);
 };
