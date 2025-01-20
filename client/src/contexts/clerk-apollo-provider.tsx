3d92d2487dfb893db899f0b669c6026fea73f764
Matt Rayford <mattrayford8686@gmail.com>
2025-01-20 11:06:27 -0600

    chore: Update sprocketforge box cover, and spacing in apollo provider

--- client/src/contexts/clerk-apollo-provider.tsx
+++ client/src/contexts/clerk-apollo-provider.tsx
@@ -1,39 +1,39 @@
 import { ReactNode, useMemo } from 'react';
 import {
-	ApolloClient,
-	ApolloProvider,
-	InMemoryCache,
-	createHttpLink,
-	from,
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+  from,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { useAuth } from '@clerk/clerk-react';
 
 const httpLink = createHttpLink({
-	uri: process.env.REACT_APP_GRAPHQL_ENDPOINT_URL,
-	credentials: 'include',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT_URL,
+  credentials: 'include',
 });
 
 export const ClerkApolloProvider = ({ children }: { children: ReactNode }) => {
-	const { getToken } = useAuth();
+  const { getToken } = useAuth();
 
-	const client = useMemo(() => {
-		const authMiddleware = setContext(async (operation, { headers }) => {
-			const token = await getToken();
+  const client = useMemo(() => {
+    const authMiddleware = setContext(async (operation, { headers }) => {
+      const token = await getToken();
 
-			return {
-				headers: {
-					...headers,
-					authorization: `Bearer ${token}`,
-				},
-			};
-		});
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${token}`,
+        },
+      };
+    });
 
-		return new ApolloClient({
-			cache: new InMemoryCache(),
-			link: from([authMiddleware, httpLink]),
-		});
-	}, [getToken]);
+    return new ApolloClient({
+      cache: new InMemoryCache(),
+      link: from([authMiddleware, httpLink]),
+    });
+  }, [getToken]);
 
-	return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
